5add7f3f00a57fb8b4e7431d14c7c68d3bda6d9b
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-09-05 10:41:12 +0700

    mencoba

--- MyFirstJavaGithub20.java
+++ MyFirstJavaGithub20.java
@@ -1,5 +1,5 @@
 public class MyFirstJavaGithub20 {
     public static void main(String[] args) {
-        System.out.println("Hello from Ekal wtih love muaccch");
+        System.out.println("Hello");
     }
 }
